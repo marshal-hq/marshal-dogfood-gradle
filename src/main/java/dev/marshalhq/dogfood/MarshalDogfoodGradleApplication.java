b1b561e76db01c77e81d35c4b712e92fe84bc6bb
Muhammad Usman <MUsman@cleopatraenterprise.com>
2026-06-19 22:45:07 +0200

    Initial Spring Boot Gradle project with javax.activation 1.1-rev-1

---
+++ src/main/java/dev/marshalhq/dogfood/MarshalDogfoodGradleApplication.java
@@ -0,0 +1,13 @@
+package dev.marshalhq.dogfood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarshalDogfoodGradleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarshalDogfoodGradleApplication.class, args);
+	}
+
+}
